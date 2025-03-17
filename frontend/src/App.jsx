5992a0b21900859402c6607af1edcfda53859632
PrabhathAbewickrama <abewickramaprabhath@gmail.com>
2025-03-17 17:50:39 +0530

    home page
    home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./assets/pages/home";
+import About from "./assets/pages/about";
+import Header from "./assets/components/Header";
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+    <header/>
+      <Routes>
+        <Route path="/" element={<Header />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
