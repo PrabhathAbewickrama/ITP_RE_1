e8cfb314156175845a4c45f17a0f325d2181e0b0
PrabhathAbewickrama <abewickramaprabhath@gmail.com>
2025-03-17 19:30:30 +0530

    header component
    header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./assets/pages/home";
 import About from "./assets/pages/about";
 import Header from "./assets/components/Header";
+
 export default function App() {
   return (
     <BrowserRouter>
-    <header/>
+    <Header />
       <Routes>
-        <Route path="/" element={<Header />} />
+    
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
--- frontend/src/assets/components/Header.jsx
+++ frontend/src/assets/components/Header.jsx
@@ -5,31 +5,40 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <header className="bg-slate-200 shadow-md">
-        <div className="flex justify-between items-center mx-auto p-4">
-            <Link to='/'>
+        <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+         <Link to='/'>   
             <h1 className="front-bold text-sm sm:text-xl flex-wrap">
             <span className='text-slate-500'>Pawfect</span>
             <span className='text-slate-900'>care</span>
         </h1>
         </Link>
-        <form className="bg-slate-500 p-2 rounded-lg flex items-center">
+        <form className="bg-slate-500 p-3 rounded-lg flex items-center">
             <input type="text" placeholder='Search...' 
             className='bg-transparent focus:outline-none w-24 sm:w-64'/>
             <FaSearch className='text-slate-500'/>
         </form>
         <ul className='flex gap-4'>
-            <link to = '/'>
+        <Link to='/'>
             <li className='hidden sm:inline text-slate-700 hover:underline'>
                 Home
                 </li>
-            </link>
-            <link to = '/about'>
+        </Link>    
+         <Link to='/about'>   
             <li className='hidden sm:inline text-slate-700 hover:underline'>
                 About
                 </li>
-                </link>
+            </Link>    
         </ul>
         </div>
     </header>
   );
 }
+
+/*import React from 'react'
+
+export default function Header() {
+  return (
+    <div>Header</div>
+  )
+}
+*/
