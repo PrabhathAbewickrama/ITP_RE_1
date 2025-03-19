50638258ac72fcbb124efa786058a7d083f0c89b
mohamedsifran <sifran2003@gmail.com>
2025-03-19 14:14:35 +0530

    create test api route

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,14 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
+mongoose
+.connect(process.env.MONGO)
+.then(() => {
     console.log('Connected to MongoDB');
-    }).catch((error) => {
-    console.log(error);
     })
+    .catch((error) => {
+        console.log(error);
+    });
 
 
 const app = express();
@@ -15,4 +18,6 @@ const app = express();
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
     }
-)
+);
+
+app.use("/backend/user", userRoutes);
